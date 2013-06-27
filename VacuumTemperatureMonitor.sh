11cdf00415218ccc21f999cdb439b2ad2c58589e
IMBL-3B-CONTROL <antonmx@gmail.com>
2013-06-27 14:52:15 +1000

    modified:   VacuumTemperatureMonitor.sh

--- VacuumTemperatureMonitor.sh
+++ VacuumTemperatureMonitor.sh
@@ -5,7 +5,7 @@ TIMESTAMP=$(date +%Y_%m_%d__%H_%M_%S)
 TimeScanMX \
 	-m 1.0e-09 -M 1.0e-05 -i 0.5 -p 1200 \
 	-d /home/ics/Desktop/Scandata/ -f "vacuum__1_$TIMESTAMP.dat" \
-	-cslg --collapse \
+	-cslg --collapse --nostore \
         SR08FE01CCG01:PRESSURE_MONITOR \
         SR08FE01CCG02:PRESSURE_MONITOR \
         SR08FE01CCG03:PRESSURE_MONITOR \
@@ -19,7 +19,7 @@ TimeScanMX \
 TimeScanMX \
 	-m 1.0e-09 -M 1.0e-05 -i 0.5 -p 1200 \
 	-d /home/ics/Desktop/Scandata/ -f "vacuum__2_$TIMESTAMP.dat" \
-	-cslg --collapse \
+	-cslg --collapse --nostore \
         SR08ID01CCG04:PRESSURE_MONITOR \
         SR08ID01CCG05:PRESSURE_MONITOR \
         SR08ID01PMG01:PRESSURE \
@@ -33,7 +33,7 @@ TimeScanMX \
 TimeScanMX \
 	--automin --automax -i 0.5 -p 1200 \
 	-d /home/ics/Desktop/Scandata/ -f "temperature__$TIMESTAMP.dat"  \
-	-csg --collapse \
+	-csg --collapse --nostore \
 	SR08FE01TES02:TEMPERATURE_MONITOR  \
 	SR08FE01TES03:TEMPERATURE_MONITOR  \
 	SR08FE01TES04:TEMPERATURE_MONITOR  \
