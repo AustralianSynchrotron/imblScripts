c450e2e42be6dc827db107587a06cbf233749ab5
antonmx <antonmx@gmail.com>
2017-03-22 10:06:28 +1100

    new file:   ctgui/postAq.WidePix.sh 	modified:   ctgui/preAq.WidePix.sh

--- ctgui/preAq.WidePix.sh
+++ ctgui/preAq.WidePix.sh
@@ -10,6 +10,8 @@ fi
 #  exit 1
 #fi
 
-ssh imbl@10.123.10.30 "echo MULTCAP $1 $DETAQNUM $PWD ${DETFILENAME}_%1.tif | nc localhost 8080"
+#ssh imbl@10.123.10.30 "echo MULTCAP $1 $DETAQNUM $PWD ${DETFILENAME}_%1.tif | nc localhost 8080"
 #ssh imbl@10.123.10.41 "echo MULTCAP $1 $DETAQNUM $2 ${DETFILENAME}_%1.tif | nc localhost 8080"
-caput SR08ID01SST01:ROTATION.STOP 1 
+echo MULTCAP $1 $DETAQNUM $2 ${DETFILENAME}_%1.tif | nc localhost 8080
+caput SR08ID01SST01:ROTATION.STOP 1
+killall -9 postAq.WidePix.sh 
